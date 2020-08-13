35fd619e7047ec330f6d85aabaa60d13a5d6c248
Ubuntu <ubuntu@academysdu-instance3.c.praqma-training.internal>
2020-08-13 12:36:21 +0000

    Works now?

--- ci/deploy_test.sh
+++ ci/deploy_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-'scp -o StrictHostKeyChecking=no ./ca-project-test.tar ubuntu@34.76.76.30:/home/ubuntu/test/ca-project-test.tar'
-'scp -o StrictHostKeyChecking=no ./docker-compose.yml ubuntu@34.76.76.30:/home/ubuntu/test'
+'scp -o StrictHostKeyChecking=no ../ca-project-test.tar ubuntu@34.76.76.30:/home/ubuntu/test/ca-project-test.tar'
+'scp -o StrictHostKeyChecking=no ../docker-compose.yml ubuntu@34.76.76.30:/home/ubuntu/test'
 'ssh -o StrictHostKeyChecking=no ubuntu@34.76.76.30 "cd /home/ubuntu/test && docker load -i ca-project-test.tar"'
 'ssh -o StrictHostKeyChecking=no ubuntu@34.76.76.30 "cd /home/ubuntu/test && docker-compose up -d"'
